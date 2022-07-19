7cbe27582353fdad503c112943e8b6bd5d8c0fb8
kaushiks90 <kaushiks90@gmail.com>
2022-07-19 09:44:47 +0530

    Added config extensition to mimimalize code

---
+++ Utils/ConfigExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DatacomConsole.Utils
+{
+    public static class ConfigExtension
+    {
+        public static void AddConfiguration<T>(this IServiceCollection services, IConfiguration configuration, string configurationNode) where T : class
+        {
+            if (string.IsNullOrEmpty(configurationNode))
+            {
+                configurationNode = typeof(T).Name;
+            }
+            var instance = Activator.CreateInstance<T>();
+            new ConfigureFromConfigurationOptions<T>(configuration.GetSection(configurationNode)).Configure(instance);
+            services.AddSingleton(instance);
+        }
+    }
+}
